26f5d9bece1bce8c2e836841544305bcd4bea468
nevenall <nevenall@hotmail.com>
2019-09-05 12:25:40 -0700

    using svelte-material-ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <script>
   import Nav from "./components/Nav.svelte";
+
+  
+
   export let name;
 </script>
 
 <style>
+  /* Import App Shell fonts+icons */
+  /* TODO: may want to add italics typefaces */
+  @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+  @import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700");
+  @import url("https://fonts.googleapis.com/css?family=Roboto+Mono");
+
   span {
     color: red;
   }
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,4 @@
 <script>
-  import { fly } from "svelte/transition";
   import book from "../pages/book";
 
   import logo from "../assets/logo-192.png";
@@ -10,83 +9,37 @@
   // TODO - close on nav or <esc>
   // DONE - slide in drawer
   // TODO - raise nav event
-  // TODO - nav drawer overlay 
-  // TODO - nav drawer transistion animation. I like the bouncy bit from that blue example. 
-
+  // TODO - nav drawer overlay
+  // TODO - nav drawer transistion animation. I like the bouncy bit from that blue example.
 
   // can i make the button inside the nav and relative to it
   // so its at the right side, like a tab, and when you pull it, it slides out?
   // also the icon lines should be relative. they are currently absolute and translated.
 
+  // want the width of the nav to be 100vw at 320px screen size,
+  // down to 33vw at wide wide screen like 1800px
+
   export let segment;
 </script>
 
 <style>
-  .button {
-    position: relative;
 
-    z-index: 1002;
-    cursor: pointer;
-    width: 20px;
-    height: 14px;
-  }
-
-  .icon {
-    position: relative;
-    width: 20px;
-    height: 14px;
-    margin: auto;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 1px;
-  }
-
-  .line {
-    position: relative;
-    background-color: #333;
-    height: 2px;
-    width: 100%;
-    border-radius: 2px;
-    left: 0;
-    transition: all 0.25s cubic-bezier(0.175, 0.885, 0.32, 1.275);
-  }
-
-  .line-1 {
-    top: 0;
-  }
-
-  .line-2 {
-    top: 0;
-    bottom: 0;
-    margin: auto;
-  }
-
-  .line-3 {
-    bottom: 0;
-  }
-
-  .button.open .line-1 {
-    transform: translateY(7px) translateY(-50%) rotate(-135deg);
-  }
-
-  .button.open .line-2 {
-    opacity: 0;
-  }
-
-  .button.open .line-3 {
-    transform: translateY(-7px) translateY(50%) rotate(135deg);
+  :root {
+    --nav-width: calc(33vw + 67 * ((1800 - 100vw) / (1800 - 320)));
   }
 
   nav {
     position: fixed;
     z-index: 99;
-    width: 50vw;
+    width: max-content;
     height: 100vh;
     top: 0;
+
     -webkit-transform: translateX(-100%);
     transform: translateX(-100%);
-    border: solid 2px red;
+
+    transition: 0.5s transform;
+    transition-timing-function: cubic-bezier(0.38, 0.52, 0.37, 1.27);
   }
 
   nav.open {
@@ -94,32 +47,116 @@
     transform: none;
   }
 
-  button {
-    z-index: 1002;
+  /*!
+ * Hamburgers
+ * @description Tasty CSS-animated hamburgers
+ * @author Jonathan Suh @jonsuh
+ * @site https://jonsuh.com/hamburgers
+ * @link https://github.com/jonsuh/hamburgers
+ */
+  .hamburger {
+    padding: 15px 15px;
+    display: inline-block;
+    cursor: pointer;
+    transition-property: opacity, filter;
+    transition-duration: 0.15s;
+    transition-timing-function: linear;
+    font: inherit;
+    color: inherit;
+    text-transform: none;
+    background-color: transparent;
+    border: 0;
+    margin: 0;
+    overflow: visible;
+    opacity: 0.5;
+  }
+  .hamburger:hover {
+     opacity: 1;
+  }
+  .hamburger.open:hover {
+    opacity: 1;
+  }
+  .hamburger.open .hamburger-inner,
+  .hamburger.open .hamburger-inner::before,
+  .hamburger.open .hamburger-inner::after {
+    background-color: #000;
+  }
+
+  .hamburger-box {
+    width: 40px;
+    height: 24px;
+    display: inline-block;
     position: relative;
-    transform: translateX(50vw);
-    width: 50px;
+  }
+
+  .hamburger-inner {
+    display: block;
+    top: 50%;
+    margin-top: -2px;
+  }
+
+  .hamburger-inner,
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    width: 40px;
+    height: 4px;
+    background-color: #000;
+    border-radius: 4px;
+    position: absolute;
+    transition-property: transform;
+    transition-duration: 0.15s;
+    transition-timing-function: ease;
+  }
+  .hamburger-inner::before,
+  .hamburger-inner::after {
+    content: "";
+    display: block;
+  }
+  .hamburger-inner::before {
+    top: -10px;
+  }
+  .hamburger-inner::after {
+    bottom: -10px;
+  }
+
+  /*
+   * Arrow Turn
+   */
+  .hamburger--arrowturn.open .hamburger-inner {
+    transform: rotate(-180deg);
+  }
+  .hamburger--arrowturn.open .hamburger-inner::before {
+    transform: translate3d(8px, 0, 0) rotate(45deg) scale(0.7, 1);
+  }
+  .hamburger--arrowturn.open .hamburger-inner::after {
+    transform: translate3d(8px, 0, 0) rotate(-45deg) scale(0.7, 1);
+  }
+
+  button {
+    z-index: 100;
+    transform: translateX(20vw);
+    /* transform: translateX(calc(33vw + 67 * ((1800 - 100vw) / (1800 - 320)))); */
+    /* transform: translateX(calc(33vw + 67 * ((1800 - 100vw) / (1800 - 320)))); */
+    /* transform: translateX(50%); */
   }
 
   button.open {
-    /* we can either translate the button with the menu or leave it where it is. I think I 
-     like leaving it where it is. 
-     it's easier to undo
-     */
-    /* transform: translateX(50vw); */
-    transform: none;
+    /* transform: translateX(var(--nav-width)); */
+    /* transform: translateX(calc(33vw + 67 * ((1800 - 100vw) / (1800 - 320)))); */
   }
 </style>
 
-<!-- <div class="button" class:open on:click={() => (open = !open)}>
-    <span class="icon">
-      <span class="line line-1" />
-      <span class="line line-2" />
-      <span class="line line-3" />
-    </span>
-  </div> -->
 <nav class:open>
-  <button class:open on:click={() => (open = !open)}>Open ></button>
+<button
+  class="hamburger hamburger--arrowturn"
+  type="button"
+  class:open
+  on:click={() => (open = !open)}>
+  <span class="hamburger-box">
+    <span class="hamburger-inner" />
+  </span>
+</button>
+
   <img src={logo} alt="logo" />
   <ul>
     {#each book.sections as section}
